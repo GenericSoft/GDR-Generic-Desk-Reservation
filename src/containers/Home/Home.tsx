347c0a2abd84965267e4e7eb8c08fe2b805efb72
NadyaPeycheva <peicheva98@abv.bg>
2023-05-17 09:14:24 +0300

    nav

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,14 +1,10 @@
 import Alert from 'react-bootstrap/Alert';
-import Navigation from '../../components/Navigation/Navigation';
 
 function Home() {
   return (
-    <>
-      <Navigation />
-      <Alert className="alert-primary">
-        This is a primary alert—check it out!
-      </Alert>
-    </>
+    <Alert className="alert-primary">
+      This is a primary alert—check it out!
+    </Alert>
   );
 }
 
