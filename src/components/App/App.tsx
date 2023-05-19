12dda0772bcfaa889db83a0f7d08669a0b175ef6
NadyaPeycheva <peicheva98@abv.bg>
2023-05-19 17:38:21 +0300

    remove needless things

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 // import ThemeProvider from '@mui/material/styles/ThemeProvider';
 // Router
 import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 // Style
 // import theme from '../../utils/muiTheme';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,13 +12,19 @@ import './App.scss';
 import Login from '../../pages/Login/Login';
 import Dashboard from '../../pages/Dashboard/Dashboard';
 
+import { RootStateType } from '../../redux/reducers/rootReducer';
+
 function App() {
+  const isHaveUser = useSelector((state: RootStateType) => state.user);
+
   return (
     // <ThemeProvider theme={theme}>
     <div className="App">
       <Routes>
+        {isHaveUser && <Route path="/" element={<Dashboard />} />}
+        {/* <Route path="/" element={<Registration />} /> */}
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {isHaveUser && <Route path="/dashboard" element={<Dashboard />} />}
       </Routes>
     </div>
     // </ThemeProvider>
