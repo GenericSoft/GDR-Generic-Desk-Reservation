8755d3917524c8668c0c17e738e685fdd6febbd2
NadyaPeycheva <peicheva98@abv.bg>
2023-05-25 18:11:34 +0300

    create logout function

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,24 +1,19 @@
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { Container } from 'react-bootstrap';
+
+import { logoutUser } from '../../redux/reducers/userReducer';
+import { useAppDispatch } from '../../redux/store';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import {
   faUser,
-  faGear,
-  faBell,
-  faEnvelopeOpen,
-  faUserGroup,
-  faImage,
-  faComments,
   faRightFromBracket,
   faFolder,
-  faPeopleGroup,
   faChartLine,
 } from '@fortawesome/free-solid-svg-icons';
 
 import './DropDown.scss';
-import { Container } from 'react-bootstrap';
 import UserImage from '../UserImage/UserImage';
 
 type User = {
@@ -29,6 +24,10 @@ type User = {
 };
 
 const DropDown = (user: User) => {
+  const dispatch = useAppDispatch();
+  const logoutFunc = async () => {
+    await dispatch(logoutUser());
+  };
   return (
     <NavDropdown id="nav-dropdown" className="drop-down" title="" align="end">
       <NavDropdown.Item className="drop-down__profile-item">
@@ -46,43 +45,16 @@ const DropDown = (user: User) => {
       <NavDropdown.Item href="#">
         <FontAwesomeIcon icon={faUser} className="drop-down__icon" /> Profile
       </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faGear} className="drop-down__icon" /> Acount
-      </NavDropdown.Item>
+
       <NavDropdown.Item href="#">
         <FontAwesomeIcon icon={faChartLine} className="drop-down__icon" />
-        Timeline
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faBell} className="drop-down__icon" />{' '}
-        Notifications
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        {' '}
-        <FontAwesomeIcon icon={faEnvelopeOpen} className="drop-down__icon" />
-        Messages
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faUserGroup} className="drop-down__icon" />
-        Connections
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faPeopleGroup} className="drop-down__icon" />
-        Groups
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faImage} className="drop-down__icon" />
-        Photos
-      </NavDropdown.Item>
-      <NavDropdown.Item href="#">
-        <FontAwesomeIcon icon={faComments} className="drop-down__icon" />
-        Forums
+        Dashboard
       </NavDropdown.Item>
       <NavDropdown.Item href="#">
         <FontAwesomeIcon icon={faFolder} className="drop-down__icon" />
         Documents
       </NavDropdown.Item>
-      <NavDropdown.Item href="#">
+      <NavDropdown.Item href="#" onClick={logoutFunc}>
         <FontAwesomeIcon
           icon={faRightFromBracket}
           className="drop-down__icon"
