9cc86e8b700dfcebd8cd1976156938fdaf8971af
kalinadukova <kalina984@abv.bg>
2023-05-26 14:02:34 +0300

    merge conflicts fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,7 +10,6 @@ import Registration from '../../pages/Registration/Registration';
 import Login from '../../pages/Login/Login';
 
 import PrivateRoutes from '../../utils/PrivateRoutes';
-import TimeTable from '../../components/TimeTable/TimeTable';
 import Calendar from '../../pages/Calendar/Calendar';
 import ImageMapProView from '../../pages/ImageMapProView/ImageMapProView';
 import ImageMapProEditor from '../../pages/ImageMapProEditor/ImageMapProEditor';
@@ -21,13 +20,14 @@ function App() {
       <Routes>
         <Route element={<PrivateRoutes />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/view" element={<ImageMapProView />} />
+          <Route path="/editor" element={<ImageMapProEditor />} />
+          <Route path="/*" element={<Navigate to="/dashboard" />} />
         </Route>
         <Route path="/*" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/view" element={<ImageMapProView />} />
-        <Route path="/dashboard" element={<TimeTable />} />
       </Routes>
     </div>
   );
