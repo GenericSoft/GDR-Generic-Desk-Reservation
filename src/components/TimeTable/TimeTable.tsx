53f81be1342e86d14ead445af72a43812a1996dd
ZarinaPaunova <zarina.valentinova@gmail.com>
2023-05-31 13:22:40 +0300

    add click func for desks in viewer, show reserved desks in viewer, show is user working from home or not in dashboard, save and fetch reservations in firestore

--- src/components/TimeTable/TimeTable.tsx
+++ src/components/TimeTable/TimeTable.tsx
@@ -5,25 +5,52 @@ import './TimeTable.scss';
 import { useEffect, useState } from 'react';
 import { DocumentData } from '@firebase/firestore';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faHouseUser,
-  faBuilding,
-  faGamepad,
-} from '@fortawesome/free-solid-svg-icons';
+import { faHouseUser, faBuilding } from '@fortawesome/free-solid-svg-icons';
 import { Spinner } from 'react-bootstrap';
+import { getAllReservations } from '../../api/reservationDeskBackend/calendarApi';
+import { format, startOfWeek, add } from 'date-fns';
 
 const TimeTable = () => {
   const [users, setUsers] = useState<DocumentData>([]);
-
+  const [reservations, setReservations] = useState<DocumentData>([]);
+  const [shownWeek, setShownWeek] = useState(new Date());
+  const [week, setWeek] = useState<string[]>([]);
   const fetchUsers = async () => {
     const users = await getAllUsers();
     if (users) {
       setUsers(users);
     }
   };
+
+  const fetchReservations = async () => {
+    const deskReservations = await getAllReservations();
+    if (deskReservations) {
+      setReservations(deskReservations);
+    }
+  };
+
+  //when user clicks on < > buttons, show the chosen week days
+  const getDaysForTheChosenWeek = () => {
+    const startDate = startOfWeek(shownWeek, { weekStartsOn: 1 });
+    const currentWeek = [];
+    let tomorrowFns = startDate;
+    for (let i = 1; i <= 7; i++) {
+      currentWeek.push(format(tomorrowFns, 'dd-MMM-y'));
+      tomorrowFns = add(tomorrowFns, {
+        days: 1,
+      });
+    }
+    setWeek(currentWeek);
+  };
+
   useEffect(() => {
     fetchUsers();
+    fetchReservations();
   }, []);
+
+  useEffect(() => {
+    getDaysForTheChosenWeek();
+  }, [shownWeek]);
 
   return (
     <>
@@ -36,63 +63,63 @@ const TimeTable = () => {
               colSpan={7}
               className="timetable-header-container pt-0 pb-0 pe-0 ps-0"
             >
-              <TimeTableHeader />
+              <TimeTableHeader onShowWeek={setShownWeek} />
             </td>
           </tr>
-          {users.docs ? (
+          {/* print all users in table  */}
+          {users.docs && reservations.docs ? (
             users.docs.map((user: DocumentData) => {
               return (
                 <tr key={user.id}>
                   <td>{user.data().firstName}</td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faHouseUser}
-                      style={{ color: '#00c9b9' }}
-                      size="lg"
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faBuilding}
-                      size="lg"
-                      style={{ color: '#724d7e' }}
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faBuilding}
-                      size="lg"
-                      style={{ color: '#724d7e' }}
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faBuilding}
-                      size="lg"
-                      style={{ color: '#724d7e' }}
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faHouseUser}
-                      style={{ color: '#00c9b9' }}
-                      size="lg"
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faGamepad}
-                      size="lg"
-                      style={{ color: '#808080' }}
-                    />
-                  </td>
-                  <td className="text-center">
-                    <FontAwesomeIcon
-                      icon={faGamepad}
-                      size="lg"
-                      style={{ color: '#808080' }}
-                    />
-                  </td>
+                  {/* print all days from chosen week in table  */}
+                  {week.map((dayFromWeek) => {
+                    const usersWithReservationForCurrDeskInCurrDay: string[] =
+                      [];
+                    // check which days in the chosen week have reservations
+                    reservations.docs.map((doc: DocumentData) => {
+                      if (doc.data().day == dayFromWeek) {
+                        // check which users have reservations for current day in the chosen week
+                        doc.data().desks.forEach((desk: DocumentData) => {
+                          desk.usersArray.forEach((user: DocumentData) => {
+                            usersWithReservationForCurrDeskInCurrDay.push(
+                              user.userId
+                            );
+                          });
+                        });
+                      }
+                    });
+                    if (
+                      reservations.docs.filter(
+                        (doc: DocumentData) => doc.data().day == dayFromWeek
+                      ).length &&
+                      usersWithReservationForCurrDeskInCurrDay.filter(
+                        (userSavedId: string) => userSavedId == user.id
+                      ).length
+                    ) {
+                      // if this user has reservation on the current day, show the building icon
+                      return (
+                        <td className="text-center" key={dayFromWeek}>
+                          <FontAwesomeIcon
+                            icon={faBuilding}
+                            size="lg"
+                            style={{ color: '#724d7e' }}
+                          />
+                        </td>
+                      );
+                    } else {
+                      // if this user has no reservation, print the house icon
+                      return (
+                        <td className="text-center" key={dayFromWeek}>
+                          <FontAwesomeIcon
+                            icon={faHouseUser}
+                            style={{ color: '#00c9b9' }}
+                            size="lg"
+                          />
+                        </td>
+                      );
+                    }
+                  })}
                 </tr>
               );
             })
