69ef589db73aed4fc01025eade02dda4d40ad505
NadyaPeycheva <peicheva98@abv.bg>
2023-06-06 12:37:32 +0300

    create edit profile function

--- src/api/reservationDeskBackend/userApi.ts
+++ src/api/reservationDeskBackend/userApi.ts
@@ -3,9 +3,13 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from 'firebase/auth';
-import { registerUserType, firebaseUserType } from '../../interfaces/User';
+import {
+  registerUserType,
+  firebaseUserType,
+  updateUserType,
+} from '../../interfaces/User';
 import { auth, db } from '../../firebase';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
 import { toError } from '../../utils/error';
 
 export const registerUserRequest = async (userData: registerUserType) => {
@@ -81,6 +85,23 @@ export const loginUserRequest = async (userData: {
       profilePic: userInfo?.profilePic,
     };
     return user;
+  } catch (error) {
+    throw toError(error, true);
+  }
+};
+
+export const editUserRequest = async (userData: updateUserType) => {
+  const { userId, newFields } = userData;
+  const userDoc = doc(db, 'users', userId);
+  try {
+    await updateDoc(userDoc, newFields);
+    const userInfo = await retrieveUserInformationRequest(userId);
+    const updatedUser = {
+      firstName: userInfo && userInfo.firstName,
+      lastName: userInfo && userInfo.lastName,
+      jobRole: userInfo && userInfo.jobRole,
+    };
+    return updatedUser;
   } catch (error) {
     throw toError(error, true);
   }
