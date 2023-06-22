cf1b5741bf94d901096ef9377d5cb3100fe926e9
NadyaPeycheva <peicheva98@abv.bg>
2023-06-22 22:44:42 +0300

    upload the user photo  when he click on save button; create modal with message when user wants to delead the photo

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { storage } from '../../firebase';
-import { deleteObject } from 'firebase/storage';
-
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { ref, deleteObject } from 'firebase/storage';
 
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -25,66 +23,50 @@ import UserImage from '../UserImage/UserImage';
 import { toError } from '../../utils/error';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { editUser } from '../../redux/reducers/userReducer';
+import ModalComponent from '../Modal/Modal';
 
 type ProfileCardProps = {
   isProfileInEditMode: boolean;
   activeEditClick: (prop: boolean) => void;
+  setChooseImage: (prop: File) => void;
+  setInstantPhoto: (prop: string) => void;
+  setLoading: (prop: string) => void;
+  loading: string;
+  instantPhoto: string;
 };
-let initialImageState: File;
 
 const ProfileCard = (props: ProfileCardProps) => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
-  const [chooseImage, setChooseImage] = useState(initialImageState);
-  const [loading, setLoading] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const editClick = () => {
-    setLoading('');
+    props.setLoading('');
     props.activeEditClick(false);
   };
 
   const chooseImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target && e.target.files) {
-      setChooseImage(e.target.files[0]);
+      const reader = new FileReader();
+      props.setChooseImage(e.target.files[0]);
+
+      reader.onload = (e) => {
+        if (e.target) {
+          props.setInstantPhoto(e.target.result as string);
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
     }
   };
 
-  useEffect(() => {
-    if (chooseImage) {
-      const storageRef = ref(storage, user.userId);
-      const uploadImage = uploadBytesResumable(storageRef, chooseImage);
-      setLoading('uploading is...');
-      uploadImage.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          if (progress === 100) {
-            setLoading('uploading is ' + progress + '% done');
-          }
-        },
-        (error) => {
-          throw toError(error, true);
-        },
-        () => {
-          getDownloadURL(uploadImage.snapshot.ref).then((downLoadURl) => {
-            const data = {
-              userId: user.userId,
-              newFields: {
-                profilePic: downLoadURl,
-              },
-            };
-            dispatch(editUser(data));
-          });
-        }
-      );
-    }
-    setChooseImage(initialImageState);
-  }, [chooseImage]);
+  const modalForDeleadImage = (state: boolean) => {
+    setShowModal(state);
+  };
+  const confirmDeleteImage = () => {
+    setShowModal(false);
 
-  const deleteImage = () => {
-    setLoading('');
+    props.setLoading('');
     const storageRef = ref(storage, user.userId);
     try {
       deleteObject(storageRef).then(() => {
@@ -102,79 +84,92 @@ const ProfileCard = (props: ProfileCardProps) => {
   };
 
   return (
-    <Card className="profile-card-container">
-      <Container className="user-data-container">
-        <UserImage
-          firstName={user.firstName || ''}
-          lastName={user.lastName || ''}
-          userImage={user.profilePic || ''}
+    <>
+      {user.profilePic !== '' && (
+        <ModalComponent
+          isShowModal={showModal}
+          confirmDeleteImage={confirmDeleteImage}
+          modalForDeleadImage={modalForDeleadImage}
+          title="Delead user image"
+          description="Are you sure to delead your photo"
         />
-        <Card.Title className="user-data-container__title">{`${user.firstName} ${user.lastName}`}</Card.Title>
-        <Card.Text className="user-data-container__email">
-          {user.email}
-        </Card.Text>
-        {!props.isProfileInEditMode && (
-          <Row className="user-data-container__buttons">
-            <span className="user-data-container__buttons--text">
-              {loading}
-            </span>
-            <Container>
-              <label
-                htmlFor="file-upload"
-                className="user-data-container__buttons--add-btn btn-secondary"
-              >
-                <input
-                  id="file-upload"
-                  className="user-data-container__buttons--add-btn"
-                  type="file"
-                  onChange={chooseImageHandler}
-                  accept=".jpg, .jpeg, .png"
-                />
-                Add photo
-              </label>
-              <FontAwesomeIcon
-                onClick={deleteImage}
-                className="user-data-container__buttons--delete-icon"
-                icon={faCircleXmark}
-              />{' '}
-            </Container>
-          </Row>
-        )}
-      </Container>
+      )}
 
-      <Card.Body className="profile-card-body"></Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>
-          <Card.Link href="#">
-            <FontAwesomeIcon icon={faUser} className="drop-down__icon" />
-            Profile
-          </Card.Link>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Card.Link
-            href="#"
-            onClick={() => {
-              navigate('/dashboard');
-            }}
-          >
-            <FontAwesomeIcon icon={faChartLine} className="drop-down__icon" />
-            Dashboard
-          </Card.Link>
-        </ListGroup.Item>
-        <ListGroup.Item className="list-group-flush__flex-item">
-          <Card.Link href="#">
-            <FontAwesomeIcon icon={faCalendarDays} /> Recent Activity{' '}
-          </Card.Link>
-          <span>9</span>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Card.Link href="#" onClick={editClick}>
-            <FontAwesomeIcon icon={faPenToSquare} />
-            Edit profile
-          </Card.Link>
-        </ListGroup.Item>
-      </ListGroup>
-    </Card>
+      <Card className="profile-card-container">
+        <Container className="user-data-container">
+          <UserImage
+            firstName={user.firstName || ''}
+            lastName={user.lastName || ''}
+            userImage={user.profilePic || ''}
+            instantPhoto={props.instantPhoto}
+          />
+          <Card.Title className="user-data-container__title">{`${user.firstName} ${user.lastName}`}</Card.Title>
+          <Card.Text className="user-data-container__email">
+            {user.email}
+          </Card.Text>
+          {!props.isProfileInEditMode && (
+            <Row className="user-data-container__buttons">
+              <span className="user-data-container__buttons--text">
+                {props.loading}
+              </span>
+              <Container>
+                <label
+                  htmlFor="file-upload"
+                  className="user-data-container__buttons--add-btn btn-secondary"
+                >
+                  <input
+                    id="file-upload"
+                    className="user-data-container__buttons--add-btn"
+                    type="file"
+                    onChange={chooseImageHandler}
+                    accept=".jpg, .jpeg, .png"
+                  />
+                  Add photo
+                </label>
+                <FontAwesomeIcon
+                  onClick={() => modalForDeleadImage(true)}
+                  className="user-data-container__buttons--delete-icon"
+                  icon={faCircleXmark}
+                />{' '}
+              </Container>
+            </Row>
+          )}
+        </Container>
+
+        <Card.Body className="profile-card-body"></Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroup.Item>
+            <Card.Link href="#">
+              <FontAwesomeIcon icon={faUser} className="drop-down__icon" />
+              Profile
+            </Card.Link>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Card.Link
+              href="#"
+              onClick={() => {
+                navigate('/dashboard');
+              }}
+            >
+              <FontAwesomeIcon icon={faChartLine} className="drop-down__icon" />
+              Dashboard
+            </Card.Link>
+          </ListGroup.Item>
+          <ListGroup.Item className="list-group-flush__flex-item">
+            <Card.Link href="#">
+              <FontAwesomeIcon icon={faCalendarDays} /> Recent Activity{' '}
+            </Card.Link>
+            <span>9</span>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Card.Link href="#" onClick={editClick}>
+              <FontAwesomeIcon icon={faPenToSquare} />
+              Edit profile
+            </Card.Link>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </>
   );
 };
 export default ProfileCard;
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -9,12 +9,22 @@ import ProfileContent from '../../components/ProfileContent/ProfileContent';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
 
+let initialImageState: File;
+
 const Profile = () => {
   const [isProfileInEditMode, setIsProfileInEditMode] = useState(true);
+  const [chooseImage, setChooseImage] = useState(initialImageState);
+  const [instantPhoto, setInstantPhoto] = useState<string>('');
+  const [loading, setLoading] = useState('');
 
   const changeProfileEditMode = (isInEditMode: boolean) => {
+    if (isInEditMode) {
+      setChooseImage(initialImageState);
+      setInstantPhoto('');
+    }
     setIsProfileInEditMode(isInEditMode);
   };
+
   return (
     <>
       <NavigationBar />
@@ -23,12 +33,20 @@ const Profile = () => {
           <ProfileCard
             activeEditClick={changeProfileEditMode}
             isProfileInEditMode={isProfileInEditMode}
+            setChooseImage={setChooseImage}
+            setInstantPhoto={setInstantPhoto}
+            instantPhoto={instantPhoto}
+            setLoading={setLoading}
+            loading={loading}
           />
         </Container>
         <Container className="profile-container__profile-content">
           <ProfileContent
             isProfileInEditMode={isProfileInEditMode}
             cancelEditClick={changeProfileEditMode}
+            chooseImage={chooseImage}
+            setChooseImage={setChooseImage}
+            setLoading={setLoading}
           />
         </Container>
       </Container>
