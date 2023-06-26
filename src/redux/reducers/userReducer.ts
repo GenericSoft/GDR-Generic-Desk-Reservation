70b997992012e4f910356d35e412cd88542add91
NadyaPeycheva <peicheva98@abv.bg>
2023-06-26 10:38:17 +0300

    fix save function for edited user data

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -48,8 +48,6 @@ export const editUser = createAsyncThunk<ProfileInfoType, EditUserDataType>(
   'users/editUserStatus',
   async (userData) => {
     try {
-      console.log('edit');
-
       const response: ProfileInfoType = await editUserRequest(userData);
       return response;
     } catch (err) {
